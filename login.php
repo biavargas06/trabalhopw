105d2d2c5f8966855c4772a1c0c53cc2d986338f
Bianca Vargas <biancavn06@gmail.com>
2023-05-12 11:07:54 -0300

    htmls

--- login.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require('twig_carregar.php');
-
-    $erro = $_GET['erro'] ?? false;
-
-    echo $twig->render('login.html', [
-        'erro' => $erro,
-    ]);
--- novo_usuario.php
+++
@@ -1,5 +0,0 @@
-<?php
-    require('verifica_login.php');
-    require('twig_carregar.php');
-
-    echo $twig->render('novo_usuario.html');
