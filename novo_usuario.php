fc3313335618a77e850314fcabeee7ba7847145b
Bianca Vargas <biancavn06@gmail.com>
2023-05-16 13:25:13 -0300

    css

--- novo_usuario.php
+++ novo_usuario.php
@@ -1,6 +1,4 @@
-<?php
-?>
-
+<link rel="stylesheet" href="css/nu.css" >
 <form method="post" action="novo_usuario_gravar.php">
     <div>
         <input type="text" name="nome" placeholder="Nome">
@@ -9,7 +7,7 @@
         <input type="email" name="email" placeholder="Email">
     </div>
     <div>
-        <input type="passowrd" name="password" placeholder="Senha">
+        <input type="password" name="password" placeholder="Senha">
     </div>
     
     <div>
